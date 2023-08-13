db252b2f67f8683db132496868c41ede6e5d0c15
zhiyang446 <50908552+zhiyang446@users.noreply.github.com>
2023-08-13 20:04:40 +0800

    Initial commit

---
+++ src/main/java/com/example/connectdb/MySQLConnectionFX.java
@@ -0,0 +1,52 @@
+package com.example.connectdb;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLConnectionFX extends Application {
+
+    Label label;
+
+    @Override
+    public void start(Stage stage) {
+
+        label = new Label();
+
+        StackPane layout = new StackPane(label);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout, 300, 150);
+        stage.setScene(scene);
+        stage.show();
+
+        Connection conn = getConnection();
+        if (conn != null) {
+            label.setText("Connected to Database!");
+        } else {
+            label.setText("Error Connecting to Database.");
+        }
+    }
+
+    public Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/testing", "root", "zhiyang446");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
